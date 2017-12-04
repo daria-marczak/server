af5232ab3cee57918e62933f09124785ed0c62eb
Daria Marczak <31421134+daria-marczak@users.noreply.github.com>
2017-12-04 17:46:50 +0100

    Update program.js

--- program.js
+++ program.js
@@ -2,21 +2,23 @@ var http = require("http");
 var fs = require("fs");
 
 var server = http.createServer();
-server.on("request", function (request, response) {
+server.on("request", function(request, response) {
     response.setHeader("Content-Type", "text/html, charset=utf-8");
     if (request.method === "GET" && request.url === "/hello") {
         response.write("<h1>Hello world!</h1>");
         response.end();
     } else if (request.method === "GET" && request.url === "/hei") {
-            fs.readFile("index.html", "utf8", function (err, file) {
-                if (err) throw err;
-                response.setHeader("200", "Content-Type", "text/html, charset=utf-8");
-                response.write(file);
-                response.end();
-            });
+        fs.readFile("index.html", "utf8", function(err, file) {
+            if (err) throw err;
+            response.setHeader("200", "Content-Type", "text/html, charset=utf-8");
+            response.write(file);
+            response.end();
+        });
     } else {
         response.statusCode = 404;
-        response.writeHead(200, {"Content-Type": "image/png"})
+        response.writeHead(200, {
+            "Content-Type": "image/png"
+        })
         fs.readFile("./lego.png", "binary", function(err, img) {
             if (err) throw err;
             response.write(img, "binary");
